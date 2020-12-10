cbc4b4426d5dae638823720c91dc0a2aaf9c3e4c
JoseMAP-99 <55493484+JoseMAP-99@users.noreply.github.com>
2020-12-10 12:38:51 +0000

    Add files via upload

--- practica7_1/practica1.cpp
+++ practica7_1/practica1.cpp
@@ -34,9 +34,107 @@ void funcion2(int argc, char* argv[]) {
 	return;
 }
 
+void funcion3(int argc, char* argv[]) {
+	int rank, size, length, compi, message;
+	char name[MPI_MAX_PROCESSOR_NAME];
+	MPI_Init(&argc, &argv);
+	MPI_Get_processor_name(name, &length);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank < size / 2) {
+		compi = rank + size / 2;
+	} else {
+		compi = rank - size / 2;
+	}
+	
+	MPI_Status status;
+	MPI_Send(&rank, 1, MPI_INT, compi, 1, MPI_COMM_WORLD);
+	MPI_Recv(&message, 1, MPI_INT, compi, 1, MPI_COMM_WORLD, &status);
+	
+	printf("[Proceso %d] Mi compi es %d, y recibi el mensaje %d\n", rank, compi, message);
+	MPI_Finalize();
+	return;
+}
+
+void funcion4(int argc, char* argv[]) {
+	int rank, size, length, compi, message;
+	char name[MPI_MAX_PROCESSOR_NAME];
+	MPI_Init(&argc, &argv);
+	MPI_Get_processor_name(name, &length);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank < size / 2) {
+		compi = rank + size / 2;
+	}
+	else {
+		compi = rank - size / 2;
+	}
+
+	MPI_Status status[2];
+	MPI_Request request[2];
+
+	MPI_Isend(&rank, 1, MPI_INT, compi, 1, MPI_COMM_WORLD, &request[1]);
+	MPI_Irecv(&message, 1, MPI_INT, compi, 1, MPI_COMM_WORLD, &request[0]);
+	
+	printf("Haciendo 10 tareas: ");
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	MPI_Waitall(2, request, status);
+
+	printf("[Proceso %d] Mi compi es %d, y recibi el mensaje %d\n", rank, compi, message);
+	MPI_Finalize();
+	return;
+}
+
+void funcion5(int argc, char* argv[]) {
+	int rank, size, length, compi, a;
+	char name[MPI_MAX_PROCESSOR_NAME];
+	MPI_Init(&argc, &argv);
+	MPI_Get_processor_name(name, &length);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	if (rank < size / 2) {
+		compi = rank + size / 2;
+	}
+	else {
+		compi = rank - size / 2;
+	}
+
+	MPI_Status status[2];
+	MPI_Request request[2];
+
+	char messa[17];
+	char message[17];
+	a = sprintf_s(messa, "%d - Hola pepito", rank);
+	
+	MPI_Isend(&messa, 17, MPI_CHAR, compi, 1, MPI_COMM_WORLD, &request[1]);
+	MPI_Irecv(&message, 17, MPI_CHAR, compi, 1, MPI_COMM_WORLD, &request[0]);
+
+	printf("Haciendo 10 tareas: ");
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", i);
+	}
+	printf("\n");
+
+	MPI_Waitall(2, request, status);
+
+	printf("[Proceso %d] Mi compi es %d, y recibi el mensaje %s\n", rank, compi, message);
+	MPI_Finalize();
+	return;
+}
+
 int main(int argc, char* argv[])
 {
 	//funcion1(argc, argv);
-	funcion2(argc, argv);
+	//funcion2(argc, argv);
+	//funcion3(argc, argv);
+	//funcion4(argc, argv);
+	funcion5(argc, argv);
 	return 0;
 }
